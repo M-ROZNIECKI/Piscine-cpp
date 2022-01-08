c1b897ba2bb8976e8e9f9f4482828e503804166d
Maximilien Rozniecki <blueyesmax69@gmail.com>
2022-01-08 22:25:36 +0100

    Update megaphone

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,27 +6,20 @@
 /*   By: mrozniec <mrozniec@student.le-101.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/15 17:39:20 by mrozniec          #+#    #+#             */
-/*   Updated: 2021/04/06 15:32:08 by mrozniec         ###   ########lyon.fr   */
+/*   Updated: 2022/01/08 22:16:59 by mrozniec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int main(int argc, char **argv)
-{
-	int i;
-	int n;
-
-	i = 0;
+int main(int argc, char **argv) {
 	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
-	else if (argc > 1)
-		while (++i < argc)
-		{
-			n = -1;
-			while (argv[i][++n])
-				std::cout << (char)std::toupper(argv[i][n]);
-		}
-	std::cout << "\n";
-	return (0);
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else if (argc > 1) {
+		for (int i = 1; i < argc; i++)
+			for (int n = 0;  argv[i][n]; n++)
+				std::cout << (char) std::toupper(argv[i][n]);
+		std::cout << std::endl;
+	}
+	return 0;
 }
