3c7747ca6e0956d6f58deabdfdc45376dbab3db4
Maximilien Rozniecki <blueyesmax69@gmail.com>
2022-01-09 15:20:44 +0100

    Module 01 ex00

---
+++ Module 01/ex00/Zombie.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*    Zombie.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrozniec <mrozniec@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/09 14:49:18 by mrozniec          #+#    #+#             */
+/*   Updated: 2022/01/09 15:02:33 by mrozniec         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PISCINE_CPP_ZOMBIE_HPP
+#define PISCINE_CPP_ZOMBIE_HPP
+
+#include <iostream>
+
+class Zombie {
+	private:
+		const std::string name;
+
+	public:
+		void announce(void);
+
+		Zombie(std::string name);
+		~Zombie();
+};
+
+#endif //PISCINE_CPP_ZOMBIE_HPP
---
+++ Module 01/ex00/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrozniec <mrozniec@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/09 14:48:15 by mrozniec          #+#    #+#             */
+/*   Updated: 2022/01/09 15:16:38 by mrozniec         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Zombie.hpp"
+
+Zombie* newZombie( std::string name );
+void randomChump( std::string name );
+
+int main() {
+	Zombie* z = newZombie("test");
+	z->announce();
+	randomChump("1");
+	randomChump("2");
+	randomChump("3");
+
+	delete z;
+	return 0;
+}
