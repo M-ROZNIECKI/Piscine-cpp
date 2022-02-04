fac5c4841b3b3f24c97506b29991766227ff9480
Maximilien Rozniecki <blueyesmax69@gmail.com>
2022-02-04 21:35:02 +0100

    Module 05 ex03 update ToDo: Module 04 add more test with main

--- Module 05/ex03/Intern.cpp
+++ Module 05/ex03/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: mrozniec <mrozniec@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/04 18:31:48 by mrozniec          #+#    #+#             */
-/*   Updated: 2022/02/04 20:35:51 by mrozniec         ###   ########.fr       */
+/*   Updated: 2022/02/04 21:24:24 by mrozniec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,22 +24,10 @@ Form *Intern::presConstructor(const std::string &target) {
 	return new PresidentialPardonForm(target);
 }
 
-Intern::Intern() : call(), form() {
-	call[0] = "shrubbery creation";
-	call[1] = "robotomy request";
-	call[2] = "presidential pardon";
-	form[0] = &shrubConstructor;
-	form[1] = &robotConstructor;
-	form[2] = &presConstructor;
+Intern::Intern() {
 }
 
-Intern::Intern(const Intern &) : call(), form() {
-	call[0] = "shrubbery creation";
-	call[1] = "robotomy request";
-	call[2] = "presidential pardon";
-	form[0] = &shrubConstructor;
-	form[1] = &robotConstructor;
-	form[2] = &presConstructor;
+Intern::Intern(const Intern &) {
 }
 
 Intern::~Intern() {
@@ -52,6 +40,14 @@ Intern &Intern::operator=(const Intern &old) {
 }
 
 Form *Intern::makeForm(const std::string &doc, const std::string &target) {
+	std::string call[3] = {"shrubbery creation",
+						   "robotomy request",
+						   "presidential pardon"};
+
+	Form*	(* form[3])(const std::string&) = {&shrubConstructor,
+												&robotConstructor,
+												&presConstructor};
+
 	for (int i = 0; i < 3; ++i) {
 		if (doc == call[i]) {
 			std::cout << "Intern creates " << doc << std::endl;
--- Module 05/ex03/Intern.hpp
+++ Module 05/ex03/Intern.hpp
@@ -6,7 +6,7 @@
 /*   By: mrozniec <mrozniec@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/04 18:31:31 by mrozniec          #+#    #+#             */
-/*   Updated: 2022/02/04 19:20:18 by mrozniec         ###   ########.fr       */
+/*   Updated: 2022/02/04 21:13:59 by mrozniec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,6 @@
 class Intern {
 
 	private:
-		std::string call[3];
-		Form*	(* form[3])(const std::string&);
-
 		static Form*	shrubConstructor(const std::string&);
 		static Form*	robotConstructor(const std::string&);
 		static Form*	presConstructor(const std::string&);
